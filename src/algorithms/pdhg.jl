b6adc0eeef918bde16139e9b6a4757c176df4116
Eric Lin <lincheukyin_eric@yahoo.com.hk>
2021-10-05 00:38:10 +0000

    Minor cleaning up and refactoring.

--- src/algorithms/pdhg.jl
+++ src/algorithms/pdhg.jl
@@ -8,24 +8,8 @@
 
 # to be consistent with the sparse column format, we consider
 # D: R^{d×n}
+# TODO: Update the above comments
 
-# function test_pdhg()
-
-#     n, d = 1000, 500
-#     γ = 0.0
-#     D = (randn(d, n) +  ones(d, n)) /√n
-#     # b = randn(n)
-#     x_star = ones(d)
-#     b = (x_star' * D)'
-#     c = ones(d)
-#     L = 1
-#     τ, σ = 1.0/L, 1.0/L
-#     K = 100000
-#     x0 = zeros(d)
-#     y0 = zeros(n)
-#     op_X_r(x, τ) = max.(x, 0.0)
-#     pdhg(D, b, c, op_X_r, x0, y0, n, d, γ, σ, τ, L, K)
-# end
 
 function pdhg(problem::StandardLinearProgram, exitcriterion::ExitCriterion; γ=0.0, L=100.0)
     A_T, b, c = problem.A_T, problem.b, problem.c
@@ -52,14 +36,15 @@ function pdhg(problem::StandardLinearProgram, exitcriterion::ExitCriterion; γ=0
 
     while !exitflag
         y[:] = y[:] + σ * ((x_bar' * A_T)' - b)
+
         x_pre = deepcopy(x)
         x[:] = x[:]  - τ * (A_T * y[:] + c[:])
         x = prox(x, τ)
         θ = 1 / sqrt(1 + 2 * γ * τ)
         τ = θ * τ
         σ = σ / θ
-        x_bar[:] = x_bar[:] + θ * (x[:] -  x_pre[:])
 
+        x_bar[:] = x_bar[:] + θ * (x[:] -  x_pre[:])
         x_tilde[:] = x_tilde[:] + x[:]
 
         if k % exitcriterion.loggingfreg == 0
@@ -74,34 +59,8 @@ function pdhg(problem::StandardLinearProgram, exitcriterion::ExitCriterion; γ=0
         end
 
         k += 1
+
     end
 
-    return results
+    return results, profilings
 end
-
-
-# function pdhg(D, b, c, op_X_r, x0,  y0, n, d, γ, σ, τ, L, K)
-#     x_bar = x0
-#     y = deepcopy(y0)
-#     x = deepcopy(x0)
-#     x_tilde = zero(x0)
-#     modulor = 10
-#     for k = 1:K
-#         y[:] = y[:] + σ * ((x_bar' * D)' - b)
-#         x_pre = deepcopy(x)
-#         x[:] = x[:]  - τ * (D * y[:] + c[:])
-#         x = op_X_r(x, τ)
-#         θ = 1/sqrt(1+2*γ*τ)
-#         τ = θ * τ
-#         σ = σ / θ
-#         x_bar[:] = x_bar[:] + θ * (x[:] -  x_pre[:])
-#         # compute loss and save
-#         x_tilde[:] = x_tilde[:] + x[:]
-#         # @info (x_tilde/k)' * D
-#         if k % modulor == 0
-#             norm_const = norm(((x_tilde/k)' * D)' - b)
-#             @info "k:$k, PDHG constraint norm: $norm_const"
-#             modulor *= 10
-#         end
-#     end
-# end
