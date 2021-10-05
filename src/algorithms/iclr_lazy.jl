cc473e7461573785228466d99984119bf2b588f1
Eric Lin <lincheukyin_eric@yahoo.com.hk>
2021-10-05 03:24:57 +0000

    Fixed bug in iclr. Working iclr and restart.

--- src/algorithms/iclr_lazy.jl
+++ src/algorithms/iclr_lazy.jl
@@ -2,7 +2,7 @@
 #  \min_{\vx\in\gX\subset\sR^d} \max_{\vy\in\sR^n} \Big\{ L(\vx,\vy) = \vc^T\vx + r(\vx)   + \vy^T\mA\vx   - \vy^T\vb\Big\}.
 # op_X_r: (I + tau_n ∂G)^{-1}
 # γ: strong convexity
-# σ, τ, θ: sequence for update
+# σ, τ, θ_x: sequence for update
 # L: Lipschitz constant
 # to be consistent with the sparse column format, we consider
 # A_T: R^{d×n}
@@ -12,7 +12,10 @@
 function iclr_lazy(
     problem::StandardLinearProgram,
     exitcriterion::ExitCriterion;
-    γ=1.0, σ=0.0, R=10, blocksize=10)  # TODO: What is γ and σ?
+    γ=1.0, σ=0.0, R=10, blocksize=10,
+    x0=nothing,
+    y0=nothing,
+    output_xy=false)  # TODO: What is γ and σ?
 
     # Algorithm 2 from the paper
 
@@ -23,7 +26,7 @@ function iclr_lazy(
 
     _time1 = time()
     # Precomputing blocks, nzrows, sliced_A_T
-    blocks, C = compute_nnrows_for_blocks(A_T, blocksize)
+    blocks, C = compute_nzrows_for_blocks(A_T, blocksize)
     sliced_A_Ts = Array{SparseMatrixCSC{Float64, Int}}([])
     for j in 1:length(C)
         push!(sliced_A_Ts, A_T[C[j], blocks[j]])
@@ -34,7 +37,7 @@ function iclr_lazy(
     ##### Start of iclr_lazy #####
 
     # Init of ICLR_Lazy
-    K = exitcriterion.maxiter  #
+    K = exitcriterion.maxiter
     m = length(blocks)
     a = 1/(R * m)
     A = zeros(K+2)
@@ -42,14 +45,22 @@ function iclr_lazy(
     pre_a = 0.0
     idx_seq = 1:m
 
-    x0 = zeros(d)
-    y0 = zeros(n)
+    if isnothing(x0)
+        x0 = zeros(d)
+    end
+    if isnothing(y0)
+        y0 = zeros(n)
+    end
+
     q = - γ * deepcopy(x0)
     x = deepcopy(x0)
     y = deepcopy(y0)
     x_tilde = zero(x0)
-    z = deepcopy(c)
-    θ = ones(Int64, length(x0))
+    y_tilde = zero(y0)
+    z = A_T * y + c
+    θ_x = ones(Int64, length(x0))
+    θ_y = ones(Int, length(y0))
+
 
     # Log initial measure
     starttime = time()
@@ -68,7 +79,7 @@ function iclr_lazy(
         # Slice of variables based on nzrowsC[j]
         z_sliced = z[C[j]]
         q_sliced = q[C[j]]
-        Adelta_sliced = A[k] .- A[θ[C[j]]]
+        Adelta_sliced = A[k] .- A[θ_x[C[j]]]
 
         # Line 5
         q_hat = q_sliced + Adelta_sliced .* z_sliced
@@ -79,7 +90,16 @@ function iclr_lazy(
         # Line 7
         sliced_A_T = sliced_A_Ts[j]
         Delta_y = γ * m * a * ((x_hat' * sliced_A_T)' - b[blocks[j]])
+        # if output_xy
+        #     y_tilde[blocks[j]] = y_tilde[blocks[j]] + (A[k] .- A[θ_y[blocks[j]]]) .* y[blocks[j]]
+        #     y_tilde[blocks[j]] = y_tilde[blocks[j]] + (m-1) * a * Delta_y[:]
+        # end
         y[blocks[j]] = y[blocks[j]] + Delta_y
+
+        if output_xy
+            y_tilde[:] = y_tilde[:] + a * y[:]
+            y_tilde[blocks[j]] = y_tilde[blocks[j]] + (m-1) * a * Delta_y[:]
+        end
 
         # Line 8
         pre_a = a
@@ -91,7 +111,7 @@ function iclr_lazy(
         q[C[j]] = q_sliced + Adelta_sliced .* z_sliced + (a + m * pre_a) * Delta_Delta_y
 
         # Line 11
-        x_tilde[C[j]] = x_tilde[C[j]] + (A[k] .- A[θ[C[j]]]) .* x[C[j]]
+        x_tilde[C[j]] = x_tilde[C[j]] + (A[k] .- A[θ_x[C[j]]]) .* x[C[j]]
 
         # Line 9  TODO: Check with CB about the ordering
         z[C[j]] = z[C[j]] + Delta_Delta_y
@@ -100,22 +120,32 @@ function iclr_lazy(
         x[C[j]] =  prox(x0[C[j]] - 1/γ * q[C[j]], 1/γ * A[k])
 
         # Line 14
-        θ[C[j]] .= k
+        θ_x[C[j]] .= k
+        if output_xy
+            θ_y[blocks[j]] .= k
+        end
 
         # Recording progress
         if k % exitcriterion.loggingfreg == 0
-            x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-            norm_const = norm(((x_tilde_tmp/A[k])' * A_T)' - b)
+            x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ_x[:]]) .* x[:]
+            x_out = x_tilde_tmp / A[k]
+            norm_const = norm((x_out' * A_T)' - b)
 
             elapsedtime = time() - starttime
             @info "k: $(k), ICLR constraint norm: $norm_const, elapsedtime: $elapsedtime"
             logresult!(results, k, elapsedtime, norm_const)
 
             exitflag = checkexitcondition(exitcriterion, k, elapsedtime, norm_const)
+            if exitflag && output_xy
+                # y_tilde_tmp = y_tilde[:] + (A[k] .- A[θ_y[:]]) .* y[:]
+                # y_out = y_tilde_tmp / A[k]
+                y_out = y_tilde / A[k]
+                return results, x_out, y_out
+            end
         end
 
         k += 1
     end
 
-    return results, profilings
+    return results
 end
--- src/algorithms/iclr_lazy_restart.jl
+++ src/algorithms/iclr_lazy_restart.jl
@@ -1,29 +1,29 @@
 
-function iclr_lazy_restart_x_y(problem::StandardLinearProgram, exitcriterion::ExitCriterion; γ=1.0, σ=0.0, R=10, blocksize=10)
-    println("R = ", R)
+function iclr_lazy_restart_x_y(
+    problem::StandardLinearProgram,
+    exitcriterion::ExitCriterion;
+    γ=1.0, σ=0.0, R=10, blocksize=10)
+
+    # Algorithm 2 from the paper
 
     A_T, b, c = problem.A_T, problem.b, problem.c
     prox = problem.prox
 
     d, n = size(A_T)
-
     x0 = zeros(d)
     y0 = zeros(n)
 
-    blocks = []
-    C = []
-    len_b = n ÷ blocksize
-
-    for i = 1:len_b
-        push!(blocks, (1 + (i-1) * blocksize): (i * blocksize))
-        row_set = []
-        for j = 1:length(blocks[i])
-            row_set = union(row_set, rowvals(A_T[:, blocks[i][j]]))
-        end
-        push!(C, row_set)
+    _time1 = time()
+    # Precomputing blocks, nzrows, sliced_A_T
+    blocks, C = compute_nzrows_for_blocks(A_T, blocksize)
+    sliced_A_Ts = Array{SparseMatrixCSC{Float64, Int}}([])
+    for j in 1:length(C)
+        push!(sliced_A_Ts, A_T[C[j], blocks[j]])
     end
+    _time2 = time()
+    @info ("Initialization time = ", _time2 - _time1)
 
-    ##### Start of iclr_lazy_x_y
+    ##### Start of iclr_lazy_restart_x_y
 
     K = exitcriterion.maxiter  #
     m = length(blocks)
@@ -38,57 +38,79 @@ function iclr_lazy_restart_x_y(problem::StandardLinearProgram, exitcriterion::Ex
     exitflag = false
 
     while !exitflag
-
-        a = 1/(R * m)
-        A = zeros(K+2)  # TODO: Can be more efficient not creating A every restart
+        # Init of ICLR_Lazy
+        a = 1 / (R * m)
+        A = zeros(K + 2)  # TODO: Can be more efficient not creating A every restart
         A[2] = a
-        pre_a, pre_A = 0.0, 0.0
-        q = zero(x0)
+        pre_a = 0.0
         idx_seq = 1:m
-        x = deepcopy(x0)  # TODO: Can remove deepcopy()
+
+        q = zero(x0)
+        x = deepcopy(x0)
         y = deepcopy(y0)
         x_tilde = zero(x0)
         y_tilde = zero(y0)
 
-        z = A_T * y  # TODO: EL needs to understand why
-        θ = ones(Int64, length(x0))
+        z = A_T * y + c
+        θ_x = ones(Int, length(x0))
+        θ_y = ones(Int, length(y0))
+
 
         k = 2
         restartflag = false
         while !exitflag && !restartflag
-            # do inner iterations
-
+            # Line 4
             j = rand(idx_seq)
 
-            q_hat = q[C[j]] + (A[k] .- A[θ[C[j]]]) .* (z[C[j]] + c[C[j]])
+            # Slice of variables based on nzrowsC[j]
+            z_sliced = z[C[j]]
+            q_sliced = q[C[j]]
+            Adelta_sliced = A[k] .- A[θ_x[C[j]]]
 
-            x_hat = prox(x0[C[j]] - 1/γ * q_hat, 1/γ * A[k])  # TODO: Check with CB why not used
-            Delta_y = γ * m * a * ((x_hat' * A_T[C[j], blocks[j]])' - b[blocks[j]])
+            # Line 5
+            q_hat = q_sliced + Adelta_sliced .* z_sliced
+
+            # Line 6
+            x_hat = prox(x0[C[j]] - 1/γ * q_hat, 1/γ * A[k])
+
+            # Line 7 & 12
+            sliced_A_T = sliced_A_Ts[j]
+            Delta_y = γ * m * a * ((x_hat' * sliced_A_T)' - b[blocks[j]])
+            y_tilde[blocks[j]] = y_tilde[blocks[j]] + (A[k] .- A[θ_y[blocks[j]]]) .* y[blocks[j]]
+            y_tilde[blocks[j]] = y_tilde[blocks[j]] + (m-1) * a * Delta_y[:]
             y[blocks[j]] = y[blocks[j]] + Delta_y
 
-            y_tilde[:] = y_tilde[:] + a * y[:]
-            y_tilde[blocks[j]] = y_tilde[blocks[j]] + (m-1) * a * Delta_y[:]
-
+            # y_tilde[:] = y_tilde[:] + a * y[:]
+            # y_tilde[blocks[j]] = y_tilde[blocks[j]] + (m-1) * a * Delta_y[:]
+            
+            # Line 8
             pre_a = a
             a = sqrt(1 + σ * A[k] / γ)/(R * m)
-
             A[k+1] = A[k] + a
-            Delta_Delta_y = A_T[C[j], blocks[j]] * Delta_y
 
-            q[C[j]] = q[C[j]] + (A[k+1] .- A[θ[C[j]] .+ 1]) .* (z[C[j]] + c[C[j]]) + (a + m * pre_a) * Delta_Delta_y
-            x_tilde[C[j]] = x_tilde[C[j]] + (A[k] .- A[θ[C[j]]]) .* x[C[j]]
+            # Line 10
+            Delta_Delta_y = sliced_A_T * Delta_y
+            q[C[j]] = q_sliced + Adelta_sliced .* z_sliced + (a + m * pre_a) * Delta_Delta_y
+
+            # Line 11
+            x_tilde[C[j]] = x_tilde[C[j]] + (A[k] .- A[θ_x[C[j]]]) .* x[C[j]]
+
+            # Line 9  TODO: Check with CB about the ordering
             z[C[j]] = z[C[j]] + Delta_Delta_y
-            # @info "test 3:" q[C[j]]
+
+            # Line 13
             x[C[j]] =  prox(x0[C[j]] - 1/γ * q[C[j]], 1/γ * A[k])
-            θ[C[j]] .= k
+
+            # Line 14
+            θ_x[C[j]] .= k
+            θ_y[blocks[j]] .= k
 
             # Logging and checking exit condition
             if outer_k % exitcriterion.loggingfreg == 0
-
-                x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]  # Ask CB
+                x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ_x[:]]) .* x[:]
                 norm_const = norm(((x_tilde_tmp/A[k])' * A_T)' - b)
                 elapsedtime = time() - starttime
-                @info "k: $(k), ICLR constraint norm: $norm_const, elapsedtime: $elapsedtime"
+                # @info "outer_k: $(outer_k), ICLR constraint norm: $norm_const, elapsedtime: $elapsedtime"
 
                 logresult!(results, outer_k, elapsedtime, norm_const)
     
@@ -100,14 +122,18 @@ function iclr_lazy_restart_x_y(problem::StandardLinearProgram, exitcriterion::Ex
 
             # set restartflag when reached some measure
             if k % (2 * m) == 0
-                x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-                norm_const = norm(((x_tilde_tmp/A[k])' * A_T)' - b)
+                x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ_x[:]]) .* x[:]
+                y_tilde_tmp = y_tilde[:] + (A[k] .- A[θ_y[:]]) .* y[:]
+                x_out = x_tilde_tmp / A[k]
+                y_out = y_tilde_tmp / A[k]
+                # y_out = y_tilde / A[k]
+                norm_const = norm((x_out' * A_T)' - b)
                 if norm_const < 0.5 * init_norm_const
                     @info "restarting"
                     @info "k ÷ m: $(k ÷ m)"
                     @info "norm_const: " norm_const
                     
-                    x0, y0 = x_tilde_tmp/A[k], y_tilde/A[k]
+                    x0, y0 = deepcopy(x_out), deepcopy(y_out)
                     init_norm_const = norm_const
                     restartflag = true
                     break
@@ -116,236 +142,8 @@ function iclr_lazy_restart_x_y(problem::StandardLinearProgram, exitcriterion::Ex
 
             k += 1
             outer_k += 1
-
         end
-
-        
     end
 
     return results
 end
-
-
-   
-
-    
-
-
-# function iclr_lazy_restart_x(A_T, b, c, op_X_r, x0, y0, n, d, γ, σ, R, S, C, K)
-
-#     init_norm = norm((x0' * A_T)' - b)
-
-#     m = length(S)
-#     a = 1/(R * m)
-#     A = zeros(K+2)
-#     A[2] = a
-#     pre_a, pre_A = 0.0, 0.0
-#     q = zero(x0)
-#     idx_seq = 1:m
-#     x = deepcopy(x0)
-#     y = deepcopy(y0)
-#     x_tilde = zero(x0)
-#     z = zero(x0)
-#     θ = ones(Int64, length(x0))
-#     modulor = 10
-#     for k = 2:K
-
-#         j = rand(idx_seq)
-#         # @info "test 1:" q[C[j]]
-#         q_hat = q[C[j]] + (A[k] .- A[θ[C[j]]]) .* (z[C[j]] + c[C[j]])
-#         # @info q_hat
-#         x_hat = op_X_r(x0[C[j]] - 1/γ * q_hat, 1/γ * A[k])
-#         Delta_y = γ * m * a * ((x[C[j]]' * A_T[C[j], S[j]])' - b[S[j]])
-#         y[S[j]] = y[S[j]] + Delta_y
-#         pre_a = a
-#         a = sqrt(1 + σ * A[k] / γ)/(R * m)
-#         # @info "A[k]: " A[k]
-#         # @info "a: " a
-#         A[k+1] = A[k] + a
-#         Delta_Delta_y = A_T[C[j], S[j]] * Delta_y
-#         # @info "test 2:" z[C[j]]
-#         # @info "test 2 1:" c[C[j]]
-#         # @info "test 2 2:" Delta_Delta_y
-#         # @info "test 2 2:" A[k+1]
-#         q[C[j]] = q[C[j]] + (A[k+1] .- A[θ[C[j]] .+ 1]) .* (z[C[j]] + c[C[j]]) + (a + m * pre_a) * Delta_Delta_y
-#         x_tilde[C[j]] = x_tilde[C[j]] + (A[k] .- A[θ[C[j]]]) .* x[C[j]]
-#         z[C[j]] = z[C[j]] + Delta_Delta_y
-#         # @info "test 3:" q[C[j]]
-#         x[C[j]] =  op_X_r(x0[C[j]] - 1/γ * q[C[j]], 1/γ * A[k])
-#         θ[C[j]] .= k
-
-#         # x_tilde += pre_a * x
-#         # if k % modulor == 0
-#         #     x_tilde[:] = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-#         #     norm_const = norm(((x_tilde/A[k])' * A_T)' - b)
-#         #     @info "k:$(k), ICLR constraint norm: $norm_const"
-#         #     modulor *= 10
-#         # end
-
-#         if k % (2 * m) == 0
-#             x_tilde[:] = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-#             norm_const = norm(((x_tilde/A[k])' * A_T)' - b)
-#             if norm_const < 0.5 * init_norm
-#                 @info "k % m: " k ÷ m
-#                 @info "norm_const: " norm_const
-#                 return x_tilde/A[k]
-#             end
-#         end
-#     end
-#     # x_tilde[:] = x_tilde[:] + (A[K+1] .- A[θ[:]]) .* x[:]
-#     return 0
-# end
-
-# function iclr_lazy_restart_x_y(A_T, b, c, op_X_r, x0, y0, n, d, γ, σ, R, S, C, K)
-
-#     init_norm = norm((x0' * A_T)' - b)
-#     m = length(S)
-#     a = 1/(R * m)
-#     A = zeros(K+2)
-#     A[2] = a
-#     pre_a, pre_A = 0.0, 0.0
-#     q = zero(x0)
-#     idx_seq = 1:m
-#     x = deepcopy(x0)
-#     y = deepcopy(y0)
-#     x_tilde = zero(x0)
-#     y_tilde = zero(y0)
-#     # z = zero(x0)
-#     z = A_T * y
-#     θ = ones(Int64, length(x0))
-#     modulor = 10
-#     for k = 2:K
-
-#         j = rand(idx_seq)
-#         # @info "test 1:" q[C[j]]
-#         q_hat = q[C[j]] + (A[k] .- A[θ[C[j]]]) .* (z[C[j]] + c[C[j]])
-#         # @info q_hat
-#         x_hat = op_X_r(x0[C[j]] - 1/γ * q_hat, 1/γ * A[k])
-#         Delta_y = γ * m * a * ((x[C[j]]' * A_T[C[j], S[j]])' - b[S[j]])
-#         y[S[j]] = y[S[j]] + Delta_y
-
-#         y_tilde[:] = y_tilde[:] + a * y[:]
-#         y_tilde[S[j]] = y_tilde[S[j]] + (m-1) * a * Delta_y[:]
-
-#         pre_a = a
-#         a = sqrt(1 + σ * A[k] / γ)/(R * m)
-#         # @info "A[k]: " A[k]
-#         # @info "a: " a
-#         A[k+1] = A[k] + a
-#         Delta_Delta_y = A_T[C[j], S[j]] * Delta_y
-#         # @info "test 2:" z[C[j]]
-#         # @info "test 2 1:" c[C[j]]
-#         # @info "test 2 2:" Delta_Delta_y
-#         # @info "test 2 2:" A[k+1]
-#         q[C[j]] = q[C[j]] + (A[k+1] .- A[θ[C[j]] .+ 1]) .* (z[C[j]] + c[C[j]]) + (a + m * pre_a) * Delta_Delta_y
-#         x_tilde[C[j]] = x_tilde[C[j]] + (A[k] .- A[θ[C[j]]]) .* x[C[j]]
-#         z[C[j]] = z[C[j]] + Delta_Delta_y
-#         # @info "test 3:" q[C[j]]
-#         x[C[j]] =  op_X_r(x0[C[j]] - 1/γ * q[C[j]], 1/γ * A[k])
-#         θ[C[j]] .= k
-
-#         # x_tilde += pre_a * x
-#         # if k % modulor == 0
-#         #     x_tilde[:] = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-#         #     norm_const = norm(((x_tilde/A[k])' * A_T)' - b)
-#         #     @info "k:$(k), ICLR constraint norm: $norm_const"
-#         #     modulor *= 10
-#         # end
-
-#         if k % (2 * m) == 0
-#             x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-#             norm_const = norm(((x_tilde_tmp/A[k])' * A_T)' - b)
-#             func_value = c' * (x_tilde_tmp/A[k])
-#             if norm_const < 0.5 * init_norm
-#                 @info "k ÷ m: $(k ÷ m)"
-#                 @info "norm_const: $(norm_const)"
-#                 @info "func_value: $(func_value-500)"
-#                 return x_tilde_tmp/A[k], y_tilde/A[k]
-#             end
-#         end
-#     end
-#     # x_tilde[:] = x_tilde[:] + (A[K+1] .- A[θ[:]]) .* x[:]
-#     return 0
-# end
-
-
-# function PD_norm(A_T, b, c, x, y)
-#     v1 = max.(c - A_T * y, 0.0)
-#     v2 = (x' * A_T)' - b
-#     # print(vcat(v1, v2))
-#     # print(typeof(vcat(v1, v2)))
-#     return norm(vcat(v1, v2))
-# end
-
-
-# function iclr_lazy_restart_x_y_v2(A_T, b, c, op_X_r, x0, y0, n, d, γ, σ, R, S, C, K)
-
-#     init_norm = PD_norm(A_T, b, c, x0, y0)
-#     m = length(S)
-#     a = 1/(R * m)
-#     A = zeros(K+2)
-#     A[2] = a
-#     pre_a, pre_A = 0.0, 0.0
-#     q = zero(x0)
-#     idx_seq = 1:m
-#     x = deepcopy(x0)
-#     y = deepcopy(y0)
-#     x_tilde = zero(x0)
-#     y_tilde = zero(y0)
-#     # z = zero(x0)
-#     z = A_T * y
-#     θ = ones(Int64, length(x0))
-#     modulor = 10
-#     for k = 2:K
-
-#         j = rand(idx_seq)
-#         # @info "test 1:" q[C[j]]
-#         q_hat = q[C[j]] + (A[k] .- A[θ[C[j]]]) .* (z[C[j]] + c[C[j]])
-#         # @info q_hat
-#         x_hat = op_X_r(x0[C[j]] - 1/γ * q_hat, 1/γ * A[k])
-#         Delta_y = γ * m * a * ((x[C[j]]' * A_T[C[j], S[j]])' - b[S[j]])
-#         y[S[j]] = y[S[j]] + Delta_y
-
-#         y_tilde[:] = y_tilde[:] + a * y[:]
-#         y_tilde[S[j]] = y_tilde[S[j]] + (m-1) * a * Delta_y[:]
-
-#         pre_a = a
-#         a = sqrt(1 + σ * A[k] / γ)/(R * m)
-#         # @info "A[k]: " A[k]
-#         # @info "a: " a
-#         A[k+1] = A[k] + a
-#         Delta_Delta_y = A_T[C[j], S[j]] * Delta_y
-#         # @info "test 2:" z[C[j]]
-#         # @info "test 2 1:" c[C[j]]
-#         # @info "test 2 2:" Delta_Delta_y
-#         # @info "test 2 2:" A[k+1]
-#         q[C[j]] = q[C[j]] + (A[k+1] .- A[θ[C[j]] .+ 1]) .* (z[C[j]] + c[C[j]]) + (a + m * pre_a) * Delta_Delta_y
-#         x_tilde[C[j]] = x_tilde[C[j]] + (A[k] .- A[θ[C[j]]]) .* x[C[j]]
-#         z[C[j]] = z[C[j]] + Delta_Delta_y
-#         # @info "test 3:" q[C[j]]
-#         x[C[j]] =  op_X_r(x0[C[j]] - 1/γ * q[C[j]], 1/γ * A[k])
-#         θ[C[j]] .= k
-
-#         # x_tilde += pre_a * x
-#         # if k % modulor == 0
-#         #     x_tilde[:] = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-#         #     norm_const = norm(((x_tilde/A[k])' * A_T)' - b)
-#         #     @info "k:$(k), ICLR constraint norm: $norm_const"
-#         #     modulor *= 10
-#         # end
-
-#         if k % (2 * m) == 0
-#             x_tilde_tmp = x_tilde[:] + (A[k] .- A[θ[:]]) .* x[:]
-#             # norm_const = norm(((x_tilde/A[k])' * A_T)' - b)
-#             norm_const = PD_norm(A_T, b, c, x_tilde_tmp/A[k], y_tilde/A[k])
-
-#             if norm_const < 0.5 * init_norm
-#                 @info "k % m: " k ÷ m
-#                 @info "norm_const: " norm_const
-#                 return x_tilde_tmp/A[k], y_tilde/A[k]
-#             end
-#         end
-#     end
-#     # x_tilde[:] = x_tilde[:] + (A[K+1] .- A[θ[:]]) .* x[:]
-#     return 0
-# end
