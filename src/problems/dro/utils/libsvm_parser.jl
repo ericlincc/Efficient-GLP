45677ee2aae97424a6b569425491cc4e7ebf2d63
Eric Lin <lincheukyin_eric@yahoo.com.hk>
2021-11-05 22:55:06 +0000

    Adding license info to files. Removed nonrestart versions.

--- src/problems/dro/utils/libsvm_parser.jl
+++ src/problems/dro/utils/libsvm_parser.jl
@@ -1,3 +1,18 @@
+# Copyright 2021 The CLVR Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """Read a libsvm binary dataset into a sparse data matrix transposed (i.e. y *. X^T)."""
 function read_libsvm_into_yXT_sparse(filepath::String, dim_dataset::Int, num_dataset::Int)
     
